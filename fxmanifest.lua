d127463b48fcba2ef22a7d96efa2c603e78e6162
ItsFlurix <61329362+ItsFlurix@users.noreply.github.com>
2023-06-04 16:47:59 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -10,10 +10,16 @@ version '1.0.0'
 
 author 'ItsFlurix'
 
+lua54 'yes'
+
 client_script 'client.lua'
 
 shared_scripts {
     '@es_extended/imports.lua',
     '@ox_lib/init.lua',
     'config.lua',
+}
+
+server_scripts {
+    'check_update.lua',
 }
